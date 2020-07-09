7caa9d9ad4fd7e4d4596d47cc4adff1b9326a1e5
MichArceS <60787304+MichArceS@users.noreply.github.com>
2020-07-09 18:23:46 -0500

    Update Secretaria.java

--- src/Usuario/Secretaria.java
+++ src/Usuario/Secretaria.java
@@ -1,5 +1,8 @@
-public class Secretaria extends Persona {
+impot java.util.ArrayList;
 
+public class Secretaria extends Persona {
+    private ArrayLis<Cita> citas;
+    
     public void registrarCita(){}
 
     public void agendarCita(){}
