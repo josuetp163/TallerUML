14e8f2c2cd73d3863622b565695df71b14d332ca
MichArceS <60787304+MichArceS@users.noreply.github.com>
2020-07-09 19:05:47 -0500

    Terminada AreaMedica

--- src/Area/AreaMedica.java
+++ src/Area/AreaMedica.java
@@ -1,4 +1,13 @@
 package Area;
+import java.util.ArrayList;
 
 public class AreaMedica {
+  protected String especialidad;
+  protected float costo;
+  protected ArrayList<Doctor> doctores;
+  
+  public AreaMedica(String s, float c){
+    especialidad = s;
+    costo = c;
+  }
 }
